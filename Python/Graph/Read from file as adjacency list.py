4363961df23efbcb100c6476abaa548beb246178
Athul Raj K <79792953+athulrajk@users.noreply.github.com>
2021-08-14 01:14:58 +0530

    Create Read from file as adjacency list.py

---
+++ Python/Graph/Read from file as adjacency list.py
@@ -0,0 +1,22 @@
+#main
+if __name__=='__main__':
+    #open the read and write files
+    with open('Input.txt') as f:
+        #read the no of test cases
+        t_cases=int(f.readline())
+        #iterate through each test case
+        for cases in range(0,t_cases):
+            #read values for n and m
+            #since it is a f.readline() is a string of the line, it needs to be split
+            n,m=[int(i) for i in f.readline().split()]
+            #graph is represented as a adjacency list
+            # undirected graph
+            graph=[[] for i in range(0,n+1)]
+            #iterate through each edge in the file
+            for edges in range(0,m):
+                u,v,w=[int(i) for i in f.readline().split()]
+                #since it is a undirected graph
+                graph[u].append((v,w))
+                graph[v].append((u,w))
+            #just printing the graph on console
+            print(graph)
